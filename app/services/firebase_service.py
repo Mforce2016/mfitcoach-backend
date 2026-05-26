7296f0d60fae608b9b7990ecb539dedc057bd288
Javier <ginouman.jb@gmail.com>
2026-05-26 19:07:24 -0300

    Actualizado firebase

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -1,7 +1,14 @@
+import os
+import json
 import firebase_admin
+
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+cred_dict = json.loads(firebase_json)
+
+cred = credentials.Certificate(cred_dict)
 
 firebase_admin.initialize_app(cred)
 
